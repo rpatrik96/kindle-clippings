53c4a5929eb8083ce7a13891b65c3b90efa07f46
Patrik Reizinger <prmedia17@gmail.com>
2022-12-29 07:53:32 +0100

    file and output path rename

--- extract-kindle-clippings.py
+++ extract-kindle-clippings.py
@@ -35,7 +35,7 @@ import sys
 if len(sys.argv) > 1:
     infile = sys.argv[1]
 else:
-    infile = 'My Clippings.txt'
+    infile = 'My Clippings - Kindle.txt'
 
 if not os.path.isfile(infile):
     username = getpass.getuser()
@@ -46,7 +46,7 @@ if not os.path.isfile(infile):
 if len(sys.argv) > 2:
     outpath = sys.argv[2]
 else:
-    outpath = 'clippings/'
+    outpath = './'
 
 if not os.path.isdir(outpath):
     # Create output path if it doesn't exist
